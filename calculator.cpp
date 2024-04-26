a99261aaed3f62f4f100fc39fa1ab78a126feacb
Md-Shamim-Miah <shamim.miah@brainstation-23.com>
2024-04-26 11:16:53 +0600

    create main formate of calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,47 @@
-calculator
+#include<iostream>
+using namespace std;
+
+// create Function and perform operation
+
+int main()
+{
+    while(1){
+    int num1,num2;
+    char operation;
+    cout<<"Enter The Two Number : ";
+    cin>>num1>>num2;
+    cout<<"Enter The Operation : ";
+    cin>>operation;
+
+    if(operation == '+')
+    {
+
+        add(num1,num2);
+
+    }
+    else if(operation == '-')
+    {
+        subtract(num1,num2);
+
+    }
+     else if(operation == '+')
+    {
+        multiplication(num1,num2);
+
+    }
+     else if(operation == '+')
+    {
+        divide(num1,num2);
+
+    }
+    else
+    {
+        cout<<"INvalid Try"<<endl;
+    }
+
+
+    cout<<endl<<endl;
+
+    }
+
+}
