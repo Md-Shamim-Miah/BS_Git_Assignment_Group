a29b431764a14e2c9b1520067159bbd718243a47
Moozaheed <57921369+Moozaheed@users.noreply.github.com>
2024-04-29 11:33:14 +0600

    Even Odd Feature Added

--- calculator.cpp
+++ calculator.cpp
@@ -3,38 +3,75 @@ using namespace std;
 
 // create Function and perform operation
 
+void details(int num)
+{
+    if(num%2==0)
+    {
+        cout<<"( Even, ";
+    }
+    else 
+    {
+        cout<<"( Odd, ";
+    }
+
+     if(num>=0)
+    {
+        cout<<"Possitive )";
+    }
+    else
+    {
+        cout<<"Negative )";
+    }
+
+}
+
+void symbol(int number1, int number2,int number3, char symbol)
+{
+    details(number1);
+    cout<<" "<<symbol<<" ";
+    details(number2);
+    cout<<" = ";
+    details(number3);
+    cout<<endl;
+
+}
+
 void addition(int number1, int number2){
     int resultOfTwoNumbers = number1+number2;
     cout<<"The Summation of two numbers is : "<<resultOfTwoNumbers<<endl;
+    symbol(number1,number2,resultOfTwoNumbers,'+');
+    
+
 }
 
-void substraction(int num1,int num2)
+void substraction(int number1,int number2)
 {
-    int ans=num1-num2;
-    cout<<"The Substraction of the numbers "<<ans<<endl;
+    int resultOfTwoNumbers=number1-number2;
+    cout<<"The Substraction of  of two numbers is:  "<<resultOfTwoNumbers<<endl;
+    symbol(number1,number2,resultOfTwoNumbers,'-');
 }
 
 void multiplication(int number1, int number2){
-    int resultOfTwoNumbers = number1*number2;
-    cout<<"The multiplication of two numbers is : "<<resultOfTwoNumbers<<endl;
+    int resultOfTwoNumbers=number1*number2;
+    cout<<"The Multiplication of  of two numbers is:  "<<resultOfTwoNumbers<<endl;
+    symbol(number1,number2,resultOfTwoNumbers,'*');
 }
 
-void division(int num1,int num2)
-{
-    int ans=num1/num2;
-    cout<<"The Division of the numbers "<<ans<<endl;
+void division(int number1, int number2){
+    int resultOfTwoNumbers=number1*number2;
+    cout<<"The Divison of  of two numbers is:  "<<resultOfTwoNumbers<<endl;
+    symbol(number1,number2,resultOfTwoNumbers,'*');
 }
-
 int main()
 {
     
     int num1,num2;
     char operation;
-    cout<<"Enter the first number : ";
+    // cout<<"Enter the first number : ";
     cin>>num1;
-    cout<<"Enter the second number : ";
+    // cout<<"Enter the second number : ";
     cin>>num2;
-    cout<<"Enter The Operation : ";
+    // cout<<"Enter The Operation : ";
     cin>>operation;
 
     if(operation == '+')
